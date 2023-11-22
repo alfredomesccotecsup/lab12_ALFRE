eff607e66c9e3b9fa1cc1928e9c75449de165a77
Alfredo Mescco <rosendo.mescco@tecsup.edu.pe>
2023-11-21 23:36:45 -0500

    LAB_12COMPLETO

---
+++ src/app/users/user-data.ts
@@ -0,0 +1,22 @@
+export const userData = [
+  { id: 1, name: 'Elijah Turner', email: 'elijah.turner@example.com', phone: '555-2468' },
+  { id: 2, name: 'Scarlett White', email: 'scarlett.white@example.com', phone: '555-1357' },
+  { id: 3, name: 'Lucas Peterson', email: 'lucas.peterson@example.com', phone: '555-8642' },
+  { id: 4, name: 'Grace Martin', email: 'grace.martin@example.com', phone: '555-7193' },
+  { id: 5, name: 'Caleb Roberts', email: 'caleb.roberts@example.com', phone: '555-3942' },
+  { id: 6, name: 'Zoe Turner', email: 'zoe.turner@example.com', phone: '555-6218' },
+  { id: 7, name: 'Mason Phillips', email: 'mason.phillips@example.com', phone: '555-9274' },
+  { id: 8, name: 'Ava Hill', email: 'ava.hill@example.com', phone: '555-4816' },
+  { id: 9, name: 'Owen Rogers', email: 'owen.rogers@example.com', phone: '555-3765' },
+  { id: 10, name: 'Lily Perry', email: 'lily.perry@example.com', phone: '555-8429' },
+  { id: 11, name: 'Nathan Reed', email: 'nathan.reed@example.com', phone: '555-2634' },
+  { id: 12, name: 'Hailey Simmons', email: 'hailey.simmons@example.com', phone: '555-7382' },
+  { id: 13, name: 'Ethan Baker', email: 'ethan.baker@example.com', phone: '555-9150' },
+  { id: 14, name: 'Mia Foster', email: 'mia.foster@example.com', phone: '555-2468' },
+  { id: 15, name: 'Logan Carter', email: 'logan.carter@example.com', phone: '555-9274' },
+  { id: 16, name: 'Sofia Foster', email: 'sofia.foster@example.com', phone: '555-4816' },
+  { id: 17, name: 'Elijah Adams', email: 'elijah.adams@example.com', phone: '555-8429' },
+  { id: 18, name: 'Addison Turner', email: 'addison.turner@example.com', phone: '555-2634' },
+  { id: 19, name: 'Jackson Perry', email: 'jackson.perry@example.com', phone: '555-7382' },
+  { id: 20, name: 'Scarlett Hughes', email: 'scarlett.hughes@example.com', phone: '555-9150' }
+];
